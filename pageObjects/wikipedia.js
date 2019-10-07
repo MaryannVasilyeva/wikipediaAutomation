a35716c0999048b6d99ecda1505ec1e28df3e538
Maryann Vasilyeva <maryannvasilyeva@Maryanns-MacBook-Pro.local>
2019-10-07 14:31:01 -0600

    additional tests

--- pageObjects/wikipedia.js
+++ pageObjects/wikipedia.js
@@ -1,6 +1,17 @@
+var testCommands = {
+    searchInput: function(search) {
+        this
+            .waitForElementVisible('@pageContent')
+        return this
+    }
+}
+
 module.exports = {
     url: 'https://en.wikipedia.org/wiki/Main_Page',
+    commands: [testCommands],
     elements: {
-
+        //SEARCH
+        pageContent: '#content',
+        searchInput: '[name="search"]'
     }
 }
--- tests/wikipediaTest.js
+++ tests/wikipediaTest.js
@@ -1,19 +1,21 @@
-var wikipediaObject
+var wikipediaObject = {}
 module.exports = {
   beforeEach: browser => {
       wikipediaObject = browser.page.wikipedia()
       wikipediaObject.navigate()
   },  
-//   after: browser => {
-//       wikipediaObject.end()
-//   }
+  after: browser => {
+      wikipediaObject.end()
+  },
   'Testing the navbar': browser => {
 
   },
   'Testing the search': browser => {
+     wikipediaObject
+        .searchInput()
 
   },
   'Testing the account login': browser => {
-      
+
   }
 }
