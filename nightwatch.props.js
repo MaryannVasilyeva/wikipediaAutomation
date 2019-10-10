3cb5956b5c9b0621618b316a81bafb21d7f60f41
Maryann Vasilyeva <maryannvasilyeva@Maryanns-MacBook-Pro.local>
2019-10-10 10:26:45 -0600

    added the gitignore

--- nightwatch.props.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: "/Users/maryannvasilyeva/DevMountain/testing-resources/",
-    seleniumServer: "selenium-server-standalone-3.141.59.jar",
-    chromedriver: "chromedriver"
- }
